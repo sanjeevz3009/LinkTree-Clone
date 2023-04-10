97217853946c2bf7c519656463c17f2341661e99
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-04-10 18:38:09 +0100

    Copy feature added

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const shareButtons = document.querySelectorAll(".tile-share-button");
 console.log(shareButtons);
 
-function copyText(e) {
+async function copyText(e) {
     e.preventDefault();
-    const link = this.getAttribute("link")
+    const link = this.getAttribute("link");
     console.log(link);
+
+    try {
+       await  navigator.clipboard.writeText(link);
+       alert("Copied link: " + link);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 shareButtons.forEach(shareButton => shareButton.addEventListener("click", copyText));
